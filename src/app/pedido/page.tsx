f62fbf4947727dcb03f1b827950d025f4394bcea
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-24 21:30:01 -0300

    commit inicial

---
+++ src/app/pedido/page.tsx
@@ -0,0 +1,36 @@
+
+
+import Link from "next/link"
+import { List } from "../components/list/List"
+import { Counter } from "../components/counter/Counter"
+import { Button } from "../components/button/Button"
+
+export default async function Pedido() {
+  const response = await fetch("http://localhost:8000/pizza/sizes")
+  const data = await response.json()
+
+  const handleClick = () => {
+
+  }
+
+  return (
+    <>
+      <h1>Pedido</h1>
+      <div>
+        { data.map(({ id, flavours, size, slices, text, }) => (
+          <p key={id}>
+            {flavours}, {size}, {slices}, {text}
+          </p>
+        ))}
+      </div>
+    <List />
+    <div>
+      <Counter />
+    </div>
+    <Link href="/">Home</Link>
+    <div>
+          <Button onClick={handleClick}>Adicionar</Button>
+    </div>
+    </>
+  )
+}
